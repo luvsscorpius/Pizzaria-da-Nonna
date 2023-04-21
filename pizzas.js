80a9462a07df6e76ef75b2ebf3fd8f11eb38573d
and <andersonvittor1801@gmail.com>
2023-04-21 15:18:15 -0300

    Terceiro commit

---
+++ pizzas.js
@@ -0,0 +1,31 @@
+let pizzaJson = [
+    {
+        id: 1, name: 'Mussarela', img: 'img/Mussarela.png', price: 25.00,
+        sizes: ['100g', '530g', '860g'], description: 'Molho de tomate, camada dupla de mussarela e orégano'
+    },
+    {
+        id: 2, name: 'Calabresa', img: 'img/Calabresa.png', price: 26.00, sizes: ['320g',
+            '530g', '860g'], description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+    },
+    {
+        id: 3, name: 'Quatro Queijos', img: 'img/Quatro-Queijos.png', price: 28.00, sizes: ['320g',
+            '530g', '860g'], description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+    },
+    {
+        id: 4, name: 'Brasileira', img: 'img/Brasileira.png', price: 30.00, sizes: ['320g',
+            '530g', '860g'], description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+        id: 5, name: 'Portuguesa', img: 'img/Portuguesa.png', price: 29.00, sizes: ['320g',
+            '530g', '860g'], description: 'Molho de tomate, mussarela, presunto, ovos, cebola, pimentão, azeitona e orégano'
+    },
+    {
+        id: 6, name: 'Moda da casa', img: 'img/Moda-da-casa.png', price: 35.00, sizes: ['320g',
+            '530g', '860g'], description: 'Molho de tomate, mussarela, carne de sol, tomates em cubo, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+        id: 7, name: 'Banana com canela', img: 'img/Banana-com-canela.png', price: 36.00, sizes: ['320g',
+            '530g', '860g'], description: 'Mussarela, banana, canela e açúcar'
+    }
+
+]
